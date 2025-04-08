e17fe3a7ddaa003ad6a7702df258aa2bcf096ff1
HaThanhTuan <109499178+tuan04@users.noreply.github.com>
2025-04-09 02:25:51 +0700

    isActive

--- Tuan08/src/components/NavItem.jsx
+++ Tuan08/src/components/NavItem.jsx
@@ -4,7 +4,8 @@ import { NavLink } from 'react-router-dom';
 export default function NavItem(props) {
     const {content, icon, toPath} = props;
     const className = "flex text-sm text-gray-700 rounded items-center gap-2 py-1 px-2";
+    const classNameActive = "flex text-sm text-white font-bold bg-[rgb(245,76,135)] rounded items-center gap-2 py-1 px-2";
   return (
-    <NavLink to={toPath} className={className}><img src={icon}/>{content}</NavLink>
+    <NavLink to={toPath} className={({isActive}) => isActive ? classNameActive : className}><img src={icon}/>{content}</NavLink>
   )
 }
